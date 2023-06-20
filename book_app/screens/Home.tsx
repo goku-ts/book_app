459e166a6e45cfa1362d4f270dce6254b21467fe
goku-ts <hgb.mega@gmail.com>
2023-06-20 01:22:09 -0500

    added assets

--- book_app/screens/Home.tsx
+++ book_app/screens/Home.tsx
@@ -132,129 +132,7 @@ const Home : FC<bookType> = ({ navigation }) => {
     const [categories, setCategories] = React.useState(categoriesData);
     const [selectedCategory, setSelectedCategory] = React.useState(1);
 
-    function renderHeader(profile: { name: any; point: any; }) {
-        return (
-            <View style={{ flex: 1, flexDirection: 'row', paddingHorizontal: SIZES.padding, alignItems: 'center' }}>
-                {/* Greetings */}
-                <View style={{ flex: 1 }}>
-                    <View style={{ marginRight: SIZES.padding }}>
-                        <Text style={{ ...FONTS.h3, color: COLORS.white }}>Good Morning</Text>
-                        <Text style={{ ...FONTS.h2, color: COLORS.white }}>{profile.name}</Text>
-                    </View>
-                </View>
-
-                {/* Points */}
-                <TouchableOpacity
-                    style={{
-                        backgroundColor: COLORS.primary,
-                        height: 40,
-                        paddingLeft: 3,
-                        paddingRight: SIZES.radius,
-                        borderRadius: 20
-                    }}
-                    onPress={() => { console.log("Point") }}
-                >
-                    <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
-                        <View style={{ width: 30, height: 30, alignItems: 'center', justifyContent: 'center', borderRadius: 25, backgroundColor: 'rgba(0,0,0,0.5)' }}>
-                            <Image
-                                source={icons.plus_icon}
-                                resizeMode="contain"
-                                style={{
-                                    width: 20,
-                                    height: 20
-                                }}
-                            />
-                        </View>
-
-                        <Text style={{ marginLeft: SIZES.base, color: COLORS.white, ...FONTS.body3 }}>{profile.point} point</Text>
-                    </View>
-                </TouchableOpacity>
-            </View>
-        )
-    }
-
-    function renderButtonSection() {
-        return (
-            <View style={{ flex: 1, justifyContent: 'center', padding: SIZES.padding }}>
-                <View style={{ flexDirection: 'row', height: 70, backgroundColor: COLORS.secondary, borderRadius: SIZES.radius }}>
-                    {/* Claim */}
-                    <TouchableOpacity
-                        style={{ flex: 1 }}
-                        onPress={() => console.log("Claim")}
-                    >
-                        <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
-                            <Image
-                                source={icons.claim_icon}
-                                resizeMode="contain"
-                                style={{
-                                    width: 30,
-                                    height: 30
-                                }}
-                            />
-                            <Text style={{ marginLeft: SIZES.base, ...FONTS.body3, color: COLORS.white }}>Claim</Text>
-                        </View>
-                    </TouchableOpacity>
-
-                    {/* Divider */}
-                    <LineDivider />
-
-                    {/* Get Point */}
-                    <TouchableOpacity
-                        style={{ flex: 1 }}
-                        onPress={() => console.log("Get Point")}
-                    >
-                        <View
-                            style={{
-                                flex: 1,
-                                flexDirection: 'row',
-                                alignItems: 'center',
-                                justifyContent: 'center'
-                            }}
-                        >
-                            <Image
-                                source={icons.point_icon}
-                                resizeMode="contain"
-                                style={{
-                                    width: 30,
-                                    height: 30
-                                }}
-                            />
-                            <Text style={{ marginLeft: SIZES.base, ...FONTS.body3, color: COLORS.white }}>Get Point</Text>
-                        </View>
-                    </TouchableOpacity>
-
-                    {/* Divider */}
-                    <LineDivider />
-
-                    {/* My Card */}
-                    <TouchableOpacity
-                        style={{ flex: 1 }}
-                        onPress={() => console.log("My Card")}
-                    >
-                        <View
-                            style={{
-                                flex: 1,
-                                flexDirection: 'row',
-                                alignItems: 'center',
-                                justifyContent: 'center'
-                            }}
-                        >
-                            <Image
-                                source={icons.card_icon}
-                                resizeMode="contain"
-                                style={{
-                                    width: 30,
-                                    height: 30
-                                }}
-                            />
-                            <Text style={{ marginLeft: SIZES.base, ...FONTS.body3, color: COLORS.white }}>My Card</Text>
-                        </View>
-                    </TouchableOpacity>
-                </View>
-            </View>
-        )
-    }
-
+  
     function renderMyBookSection(myBooks: ArrayLike<any> | null | undefined) {
 
         type renderType = {
@@ -496,7 +374,7 @@ const Home : FC<bookType> = ({ navigation }) => {
     }
 
     return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.black }}>
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.white }}>
             {/* Header Section */}
             <StatusBar style="light"/>
 
